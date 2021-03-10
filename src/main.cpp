ba560ba1f5c665dfedb8b3712a43c46c158a43b7
Trey Stoner <trey.stoner@outlook.com>
2021-03-10 07:06:42 -0800

    hid console on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,15 @@
 
 #include "calculator.h"
 
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+  #include <Windows.h>
+#endif
+
 int main(int argc, char *argv[])
 {
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+      FreeConsole();
+    #endif
     QApplication a(argc, argv);
     MainWindow w(0, new Calculator());
     w.show();
